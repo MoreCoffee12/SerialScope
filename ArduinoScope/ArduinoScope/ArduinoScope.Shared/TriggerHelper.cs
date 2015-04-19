02060bd4292f7918eb3fb5ca62957a35bad85577
MoreCoffee12 <brian.howard@ihmail.com>
2015-04-19 11:14:24 -0700

    Included worksheet and trigger class

---
+++ ArduinoScope/ArduinoScope/ArduinoScope.Shared/TriggerHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArduinoScope
+{
+    class TriggerHelper
+    {
+    }
+
+    public enum TriggerState
+    {
+        Scan,
+        Extern,
+        Ch1,
+        Ch2
+    }
+}
